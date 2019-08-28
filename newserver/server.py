ee7da0419e1dc54e146bc27b6f12eeb275678a49
illfalcon <kostasminch@gmail.com>
2019-08-28 14:15:44 +0300

    get for past events

--- newserver/server.py
+++ newserver/server.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from .models import *
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
+import datetime
+import json
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     __tablename__ = "users"
@@ -30,6 +34,9 @@ class Event(db.Model):
     approved = db.Column(db.Integer)
     event_date = db.Column(db.Date, nullable=False)
 
+    def to_dict(self):
+        return {'id': self.id, 'url': self.url, 'article': self.article, "event_date": self.event_date.strftime('%d-%m-%Y')}
+
 
 class Landing(db.Model):
     __tablename__ = "landings"
@@ -50,10 +57,27 @@ class Webpage(db.Model):
 db.create_all()
 import newcrawler.crawler
 
-newcrawler.crawler.assess()
+# newcrawler.crawler.assess()
 
 
-@app.route("/")
-def index():
-    flights = Webpage.query.all()
-    return flights
+api = Api(app)
+
+
+@api.representation('application/json')
+def output_json(data, code, headers=None):
+    resp = make_response(json.dumps(data, ensure_ascii=False), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+
+class PastEventsResource(Resource):
+    def get(self):
+        n = datetime.datetime.now()
+        year = n.year
+        month = n.month
+        day = n.day
+        events = Event.query.filter(Event.event_date < datetime.date(year, month, day).strftime('%d-%m-%Y'), Event.approved == None).limit(20).all()
+        return {'events': [e.to_dict() for e in events]}, 200
+
+
+api.add_resource(PastEventsResource, '/api/past_events')
